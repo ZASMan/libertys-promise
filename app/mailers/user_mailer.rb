63d5757a706f6c7c403e107e1a2799c2f4ab94e1
Zane <i.zane.am.robot@gmail.com>
2016-07-12 11:55:39 -0400

    fixed line in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,8 @@ class UserMailer < ApplicationMailer
 
 	def contact_form(email, name, message)
 		@message = message
-			mail(:from => email,
-				:to => 'info@libertyspromise.org',
-				:subject => "New contact form message from #{name}")
-		end
+		mail(:from => email,
+			:to => 'info@libertyspromise.org',
+			:subject => "New contact form message from #{name}")
 	end
 end
