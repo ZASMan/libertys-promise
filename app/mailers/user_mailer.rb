7c40882dd75e1cc40b67cf84f83079d5ae667f98
Zane <i.zane.am.robot@gmail.com>
2016-07-12 11:57:01 -0400

    Changed email for testing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
 	def contact_form(email, name, message)
 		@message = message
 		mail(:from => email,
-			:to => 'info@libertyspromise.org',
+			:to => 'zsandin@libertyspromise.org',
 			:subject => "New contact form message from #{name}")
 	end
 end
