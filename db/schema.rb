430d1d94db661f0c0d3a9da99f78f23ddb8e5b83
Zane <i.zane.am.robot@gmail.com>
2016-07-12 15:39:46 -0400

    Fixed link styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712161128) do
+ActiveRecord::Schema.define(version: 20160712191237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160712161128) do
     t.string   "location"
     t.string   "position"
     t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
