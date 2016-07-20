3409dac46411c28d58617f3758ecd558511e07e1
Zane <i.zane.am.robot@gmail.com>
2016-07-20 14:25:25 -0400

    Added paypal buttons and donate now to navbar

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
 	def contact_form(email, name, message)
 		@message = message
 		mail(:from => email,
-			:to => 'rponichtera@libertyspromise.org',
+			:to => 'info@libertyspromise.org',
 			:subject => "New contact form message from #{name}")
 	end
 end
