b65b7585b34c97d7790c270ac5743b7ee9fd87cb
Zane <i.zane.am.robot@gmail.com>
2016-07-25 17:06:16 -0400

    Added validation for images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image, styles: {large: "800x800>", medium: "600x600>", thumb: "150x150#"}
+	has_attached_file :image, styles: {large: "800x800>", medium: "600x600>", small: "350x350>", thumb: "150x150#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	belongs_to :user
 
 	#Posts Must Be Filled Out
 	validates :title, presence: true
+	validates :image_file_name, presence: 'true'
 	validates :body, presence: true
 end
